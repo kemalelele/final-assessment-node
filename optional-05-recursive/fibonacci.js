6fcc8007f7d13b4cdd80edb50e9e5f96a74e8f1e
Kemal Idris <55340119+kemalelele@users.noreply.github.com>
2026-02-26 10:48:34 +0700

    Update fibonacci.js

--- optional-05-recursive/fibonacci.js
+++ optional-05-recursive/fibonacci.js
@@ -1,4 +1,17 @@
-function fibonacci(n) {}
+function fibonacci(n) {
+  if (n === 0) {
+    return [0];
+  }
+
+  if (n === 1) {
+    return [0, 1];
+  }
+
+  const series = fibonacci(n - 1);
+  const nextValue = series[series.length - 1] + series[series.length - 2];
+
+  return [...series, nextValue];
+}
 
 // Jangan hapus kode di bawah ini!
 export default fibonacci;
